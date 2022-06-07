eba57ef035e96e7fbe9b2c25c423121c49888d79
Joey Sea <joey.c.sea@gmail.com>
2022-06-06 21:42:03 -0400

    lots of edits to page, experimented with JS but did not work, and CSS styling

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+// // mobile nav bar
+// const mobileNav = document.getElementsByClassName("mobileNavButton");
+// const mobileNavButton = mobileNav[0];
+
+// const mobileNavMenu = document.getElementsByClassName("mobileNav")
+// const mobileNavSlideIn = mobileNavMenu[0];
+// console.log(mobileNavSlideIn);
+
+// mobileNavButton.addEventListner('click', function(e) {
+//    mobileNavSlideIn.classList.toggle("active");
+//    const icon = this;
+//    icon.classList.toggle('fa-bars');
+//    icon.classList.toggle('fa-x');
+// });
