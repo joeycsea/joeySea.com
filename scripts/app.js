9d9c23571c3a9fc56006d589215c650251999060
Joey Sea <joey.c.sea@gmail.com>
2022-06-09 21:39:14 -0400

    worked on media queries, fixed some CSS

--- scripts/app.js
+++ scripts/app.js
@@ -7,7 +7,7 @@ const mobileNavSlideIn = mobileNavMenu[0].children[1];
 
 mobileNavButton.addEventListener('click', function() {
    mobileNavSlideIn.classList.toggle("active");
-   const icon = this.children[1];
+   const icon = this;
    icon.classList.toggle('fa-bars');
    icon.classList.toggle('fa-x');
 });
